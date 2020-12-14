d002e96c5443d5320c24a1566b95e6309d6be481
Didac <dfc3@alumnes.udl.cat>
2020-12-14 10:21:50 +0100

    resolve csv read

--- r-scripts/mca-study.R
+++ r-scripts/mca-study.R
@@ -17,73 +17,6 @@ library(plotly)
 library(ggthemes)
 source("Scripts/mca-study.R")
 
-# ----------------------------- Auxiliar Functions ------------------------------- #
-boxplot_by_gender <- function(cancer.data, cancer) {
-  head(cancer.data)
-  df1 <- filter(cancer.data, gender == "Male")
-  df2 <- filter(cancer.data, gender == "Female")
-  
-  y_title <- list(
-    title = "Age"
-  )
-  x_title <- list(
-    title = "Gender"
-  )
-  
-  p <- plot_ly(df1, x = df1[["gender"]], y = df1[["age"]], type  = "box", name = 'Male')%>%
-    add_trace(df2, x = df2[["gender"]], y = df2[["age"]], name = 'Female') %>%
-    layout(yaxis = y_title) %>%
-    layout(xaxis = x_title)
-    #layout(title = paste("Cancer by gender -", cancer))
-  p
-}
-
-boxplot_by_population <- function(cancer.data, cancer) {
-  df1 <- filter(cancer.data, population == "Urban")
-  df2 <- filter(cancer.data, population == "Rural")
-  
-  y_title <- list(
-    title = "Age"
-  )
-  x_title <- list(
-    title = "Population"
-  )
-  
-  p <- plot_ly(df1, x = df1[["population"]], y = df1[["age"]], type  = "box", name = 'Urban')%>%
-    add_trace(df2, x = df2[["population"]], y = df2[["age"]], name = 'Rural') %>%
-    layout(yaxis = y_title, xaxis = x_title, grid = 'rgba(0,0,0,0)')
-    #layout(title = paste("Cancer by population - ", cancer))
-  p
-  p
-  
-}
-
-find_outliers <- function () {
-  cancer.data.colonrectum = read_csv("/Users/Didac/OneDrive - Generalitat de Catalunya/CANCER/MCA-Cancer/data/private/analisi-colonrecte.csv")
-  boxplot_by_gender(cancer.data.colonrectum, "Colon and Rectum")
-  boxplot_by_population(cancer.data.colonrectum, "Colon and Rectum")
-  
-  cancer.data.lung = read_csv("/Users/Didac/OneDrive - Generalitat de Catalunya/CANCER/MCA-Cancer/data/private/analisi-pulmo.csv")
-  boxplot_by_gender(cancer.data.lung, "Lung")
-  boxplot_by_population(cancer.data.lung, "Lung")
-  
-  cancer.data.urinarybladder = read_csv("/Users/Didac/OneDrive - Generalitat de Catalunya/CANCER/MCA-Cancer/data/private/analisi-bufeta.csv")
-  boxplot_by_gender(cancer.data.urinarybladder, "Urinary Bladder")
-  boxplot_by_population(cancer.data.urinarybladder, "Urinary Bladder")
-  
-  cancer.data.prostate = read_csv("/Users/Didac/OneDrive - Generalitat de Catalunya/CANCER/MCA-Cancer/data/private/analisi-prostata.csv")
-  boxplot_by_gender(cancer.data.prostate, "Prostate")
-  boxplot_by_population(cancer.data.prostate, "Prostate")
-  
-  cancer.data.breast = read_csv("/Users/Didac/OneDrive - Generalitat de Catalunya/CANCER/MCA-Cancer/data/private/analisi-mama.csv")
-  boxplot_by_gender(cancer.data.breast, "Breast")
-  boxplot_by_population(cancer.data.breast, "Breast")
-}
-
-
-#Calculate the outliers using Box Plot technique
-find_outliers()
-
 #' @details 
 #' This CSV is cleaned and contains the following columns PATIENT_ID, AGE_GROUP, POPULATION, CANCER and GENDER
 #' * POPULATION is a categorical variable with two values Urban or Rural.
@@ -91,11 +24,11 @@ find_outliers()
 #' * AGE_GROUP is a categorical variable with ithe a set of age segmentation groups.
 #' * GENDER is a categorical variable with gender information Male or Female.
 
-cancer.data=read_csv("/Users/Didac/OneDrive - Generalitat de Catalunya/CANCER/MCA-Cancer/data/private/analisisENG.csv")
+cancer.data=read_csv("../../MCA-Cancer/data/mock.csv")
 summary(cancer.data)
 table(cancer.data$gender)
-table(cancer.data$edat)
-table(cancer.data$poblacio)
+table(cancer.data$age_group)
+table(cancer.data$population)
 
 head(cancer.data)
 
@@ -378,7 +311,9 @@ fviz_mca_ind(res.mca3,
 
 #'----------------------------------------------- Study 9 - Urban ---------------------------------------------------------------------#
 
-cancer.data=read_csv("Desktop/MCA-Cancer/data/private/analisi-urba.csv")
+cancer.data=read_csv("../../MCA-Cancer/data/mock.csv")
+filter <- c("Urban")
+cancer.data %>% filter(population %in% filter) -> cancer.data
 
 cancer.data.analisi = cancer.data[, 2:4]
 res.mca.urban <- MCA(cancer.data.analisi, graph = FALSE)
@@ -406,7 +341,9 @@ fviz_mca_var(res.mca.urban, col.var = "contrib",
 
 #'----------------------------------------------- Study 9 - Rural ---------------------------------------------------------------------#
 
-cancer.data=read_csv("Desktop/MCA-Cancer/data/private/analisi-rural.csv")
+cancer.data=read_csv("../../MCA-Cancer/data/mock.csv")
+filter <- c("Rural")
+cancer.data %>% filter(population %in% filter) -> cancer.data
 
 cancer.data.analisi = cancer.data[, 2:4]
 res.mca.rural <- MCA(cancer.data.analisi, graph = FALSE)
